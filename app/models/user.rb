33f5b5ed0c7bcbf75b3c4a848082bb626c80a730
Frank DelPidio <frankdaedalus@gmail.com>
2015-07-08 14:45:53 -0400

    create friendship_relation method in User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,4 +43,8 @@ class User < ActiveRecord::Base
     end
   end
 
+  def friendship_relation(user_2)
+    Friendship.where(user_id: [self.id, user.id], friend_id: [self.id, user.id]).first
+  end
+
 end
