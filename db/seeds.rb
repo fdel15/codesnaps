f244d5bba50c408b3a63e1f27b30a31e9cd4587c
Frank DelPidio <frankdaedalus@gmail.com>
2015-07-08 13:34:06 -0400

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "fdel15", email: "frankdaedalus@gmail.com", password:"12345678", password_confirmation: "12345678")
+User.create(username: "fdel16", email: "frankdaedalus@gmail4.com", password:"12345678", password_confirmation: "12345678")
+User.create(username: "fdel17", email: "frankdaedalus@gmail3.com", password:"12345678", password_confirmation: "12345678")
+User.create(username: "fdel18", email: "frankdaedalus@gmail2.com", password:"12345678", password_confirmation: "12345678")
+User.create(username: "fdel19", email: "frankdaedalus@gmail1.com", password:"12345678", password_confirmation: "12345678")
+p "Test users created"
