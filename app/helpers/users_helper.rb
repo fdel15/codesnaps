3e0a55d128f34e24d4c6435078ff10a0569222ed
Frank DelPidio <frankdaedalus@gmail.com>
2015-07-08 14:51:39 -0400

    Helper: action_buttons cancel friendship/request links

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ module UsersHelper
   def action_buttons(user)
     case current_user.friendship_status(user)
     when "friends"
-      "Remove Friendship"
+      link_to "Cancel Friendship", friendship_path(current_user.friendship_relation(user)), method: :delete
     when "pending"
-      "Cancel Request"
+      link_to "Cancel Request", friendship_path(current_user.friendship_relation(user)), method: :delete
     when "requested"
       "Accept or Deny"
     when "not_friends"
