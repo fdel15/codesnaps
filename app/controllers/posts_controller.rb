7a0907bb5449002244c29a221f1abf8695dfe1ba
Frank DelPidio <frankdaedalus@gmail.com>
2015-07-13 20:24:30 -0400

    add create action for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,20 @@
 class PostsController < ApplicationController
 
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      respond_to do |format|
+        format.html { redirect_to user_path(@post.user.username), notice: "Post Created" }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
